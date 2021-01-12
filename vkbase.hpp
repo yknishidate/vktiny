47d9d6579852d37408cccdc8a91b187645659ec0
nishidate-yuki <asta18425@gmail.com>
2021-01-12 11:34:17 +0900

    add Device::submitCommandBuffer

--- vkbase.hpp
+++ vkbase.hpp
@@ -271,7 +271,8 @@ namespace vkray
 
         // for other objects
         uint32_t findMemoryType(const uint32_t typeFilter, const vk::MemoryPropertyFlags properties) const;
-        vk::UniqueCommandBuffer createCommandBuffer(vk::CommandBufferLevel level, bool begin);
+        vk::UniqueCommandBuffer createCommandBuffer(vk::CommandBufferLevel level, bool begin) const;
+        void submitCommandBuffer(vk::CommandBuffer& commandBuffer);
 
     private:
 
@@ -423,7 +424,7 @@ namespace vkray
             return device.getHandle().getImageMemoryRequirements(*image);
         }
 
-        void transitionImageLayout(vk::CommandPool& commandPool, vk::ImageLayout newLayout);
+        void transitionImageLayout(vk::ImageLayout newLayout);
         //void copyFrom(CommandPool& commandPool, const Buffer& buffer);
 
     private:
@@ -720,7 +721,7 @@ namespace vkray
         throw std::runtime_error("failed to find suitable memory type");
     }
 
-    vk::UniqueCommandBuffer Device::createCommandBuffer(vk::CommandBufferLevel level, bool begin)
+    vk::UniqueCommandBuffer Device::createCommandBuffer(vk::CommandBufferLevel level, bool begin) const
     {
         assert(commandPool);
 
@@ -736,6 +737,17 @@ namespace vkray
         }
 
         return commandBuffer;
+    }
+
+    void Device::submitCommandBuffer(vk::CommandBuffer& commandBuffer)
+    {
+        commandBuffer.end();
+
+        vk::UniqueFence fence = device->createFenceUnique({});
+
+        graphicsQueue.submit(vk::SubmitInfo{}.setCommandBuffers(commandBuffer), fence.get());
+
+        device->waitForFences(fence.get(), true, std::numeric_limits<uint64_t>::max());
     }
 
     // SwapChain
@@ -944,6 +956,59 @@ namespace vkray
         device.getHandle().bindImageMemory(*image, *memory, 0);
     }
 
+    void Image::transitionImageLayout(vk::ImageLayout newLayout)
+    {
+        auto commandBuffer = device.createCommandBuffer(vk::CommandBufferLevel::ePrimary, true);
+
+        //VkImageMemoryBarrier barrier = {};
+        //barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+        //barrier.oldLayout = imageLayout_;
+        //barrier.newLayout = newLayout;
+        //barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        //barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        //barrier.image = image_;
+        //barrier.subresourceRange.baseMipLevel = 0;
+        //barrier.subresourceRange.levelCount = 1;
+        //barrier.subresourceRange.baseArrayLayer = 0;
+        //barrier.subresourceRange.layerCount = 1;
+
+        //if (newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+        //    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+
+        //    if (DepthBuffer::HasStencilComponent(format_)) {
+        //        barrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+        //    }
+        //} else {
+        //    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        //}
+
+        //VkPipelineStageFlags sourceStage;
+        //VkPipelineStageFlags destinationStage;
+
+        //if (imageLayout_ == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+        //    barrier.srcAccessMask = 0;
+        //    barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+        //    sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        //    destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        //} else if (imageLayout_ == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+        //    barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        //    barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+        //    sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        //    destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+        //} else if (imageLayout_ == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+        //    barrier.srcAccessMask = 0;
+        //    barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+        //    sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        //    destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+        //} else {
+        //    Throw(std::invalid_argument("unsupported layout transition"));
+        //}
+
+        //vkCmdPipelineBarrier(commandBuffer, sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
+    }
 
 
 } // vkray
