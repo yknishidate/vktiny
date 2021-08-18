797a80d4bfb8ffe6d0eb0ee8eaeb13202df7572e
nishidate-yuki <yuki18425@gmail.com>
2021-08-18 15:52:14 +0900

    add material

--- include/vktiny/Mesh.hpp
+++ include/vktiny/Mesh.hpp
@@ -15,6 +15,20 @@ namespace vkt
         glm::vec4 tangent;
     };
 
+    struct Material
+    {
+        int baseColorTextureIndex{ -1 };
+        int metallicRoughnessTextureIndex{ -1 };
+        int normalTextureIndex{ -1 };
+        int occlusionTextureIndex{ -1 };
+        int emissiveTextureIndex{ -1 };
+
+        glm::vec4 baseColorFactor{ 1.0f };
+        float metallicFactor{ 1.0f };
+        float roughnessFactor{ 1.0f };
+        glm::vec3 emissiveFactor{ 0.0f };
+    };
+
     class Mesh
     {
     public:
