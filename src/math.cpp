e3688bed056b2c81e1174b8a25ec866ba1a8bc9d
nishidate-yuki <yuki18425@gmail.com>
2021-08-20 10:20:28 +0900

    update transform

--- src/math.cpp
+++ src/math.cpp
@@ -6,7 +6,8 @@ namespace vkt
     glm::mat4 flipY(const glm::mat4& transformMatrix)
     {
         glm::mat4 flipped = transformMatrix;
-        flipped[1][1] *= -1.0;
+        flipped[1][1] *= -1.0; // flip y scale
+        flipped[3][1] *= -1.0; // flip y translate
         return flipped;
     }
 }
