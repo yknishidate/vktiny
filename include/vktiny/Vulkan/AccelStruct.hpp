be12e69ca8f29c7243dc90e1387d96bd9a6b4210
nishidate-yuki <yuki18425@gmail.com>
2021-08-20 10:12:42 +0900

    update

--- include/vktiny/Vulkan/AccelStruct.hpp
+++ include/vktiny/Vulkan/AccelStruct.hpp
@@ -21,82 +21,19 @@ namespace vkt
         const auto& get() const { return accelStruct; }
         auto getDeviceAddress() const { return deviceAddress; }
 
-        vk::WriteDescriptorSet createWrite()
-        {
-            asInfo = { *accelStruct };
-            vk::WriteDescriptorSet asWrite;
-            asWrite.setDescriptorCount(1);
-            asWrite.setPNext(&asInfo);
-            return asWrite;
-        }
+        vk::WriteDescriptorSet createWrite();
 
-        void initialize(const Context& context)
-        {
-            this->context = &context;
-        }
+        void initialize(const Context& context);
 
         void build(vk::AccelerationStructureGeometryKHR& geometry,
                    const vk::AccelerationStructureTypeKHR& type,
-                   uint32_t primitiveCount)
-        {
-            this->type = type;
-            this->primitiveCount = primitiveCount;
-
-            buildGeometryInfo.setType(type);
-            buildGeometryInfo.setFlags(vk::BuildAccelerationStructureFlagBitsKHR::ePreferFastTrace);
-            buildGeometryInfo.setGeometries(geometry);
-
-            getSizes();
-            createBuffer();
-            createAccelStruct();
-            createScratchBuffer();
-
-            buildGeometryInfo.setDstAccelerationStructure(*accelStruct);
-            buildGeometryInfo.setScratchData(scratchBuffer.getDeviceAddress());
-
-            vk::AccelerationStructureBuildRangeInfoKHR buildRangeInfo{};
-            buildRangeInfo
-                .setPrimitiveCount(primitiveCount)
-                .setPrimitiveOffset(0)
-                .setFirstVertex(0)
-                .setTransformOffset(0);
-
-            auto cmdBuf = context->getDevice().beginGraphicsCommand();
-            cmdBuf->buildAccelerationStructuresKHR(buildGeometryInfo, &buildRangeInfo);
-            context->getDevice().endGraphicsCommand(*cmdBuf);
-
-            deviceAddress = context->getVkDevice().getAccelerationStructureAddressKHR({ *accelStruct });
-        }
+                   uint32_t primitiveCount);
 
     protected:
-        void createBuffer()
-        {
-            buffer.initialize(*context, size,
-                              vkBU::eAccelerationStructureStorageKHR | vkBU::eShaderDeviceAddress,
-                              vkMP::eDeviceLocal);
-        }
-        void createAccelStruct()
-        {
-            vk::AccelerationStructureCreateInfoKHR createInfo;
-            createInfo.setBuffer(buffer.get());
-            createInfo.setSize(size);
-            createInfo.setType(type);
-            accelStruct = context->getVkDevice().createAccelerationStructureKHRUnique(createInfo);
-        }
-        void createScratchBuffer()
-        {
-            scratchBuffer.initialize(*context,
-                                     scratchSize,
-                                     vkBU::eStorageBuffer | vkBU::eShaderDeviceAddress,
-                                     vkMP::eDeviceLocal);
-        }
-        void getSizes()
-        {
-            auto buildSizesInfo = context->getVkDevice().getAccelerationStructureBuildSizesKHR(
-                vk::AccelerationStructureBuildTypeKHR::eDevice, buildGeometryInfo, primitiveCount);
-            size = buildSizesInfo.accelerationStructureSize;
-            scratchSize = buildSizesInfo.buildScratchSize;
-        }
+        void createBuffer();
+        void createAccelStruct();
+        void createScratchBuffer();
+        void getSizes();
 
         const Context* context;
 
--- src/Vulkan/AccelStruct.cpp
+++ src/Vulkan/AccelStruct.cpp
@@ -43,7 +43,9 @@ void vkt::BottomLevelAccelStruct::initialize(const Context& context, const Mesh&
                mesh.getIndices(), mesh.getIndexBuffer());
 }
 
-void vkt::TopLevelAccelStruct::initialize(const Context& context, const BottomLevelAccelStruct& bottomLevelAS, const glm::mat4& transform)
+void vkt::TopLevelAccelStruct::initialize(const Context& context,
+                                          const BottomLevelAccelStruct& bottomLevelAS,
+                                          const glm::mat4& transform)
 {
     AccelStruct::initialize(context);
 
@@ -107,4 +109,81 @@ void vkt::TopLevelAccelStruct::initialize(const Context& context,
     geometry.setFlags(vk::GeometryFlagBitsKHR::eOpaque);
 
     AccelStruct::build(geometry, vk::AccelerationStructureTypeKHR::eTopLevel, instances.size());
+}
+
+vk::WriteDescriptorSet vkt::AccelStruct::createWrite()
+{
+    asInfo = { *accelStruct };
+    vk::WriteDescriptorSet asWrite;
+    asWrite.setDescriptorCount(1);
+    asWrite.setPNext(&asInfo);
+    return asWrite;
+}
+
+void vkt::AccelStruct::initialize(const Context& context)
+{
+    this->context = &context;
+}
+
+void vkt::AccelStruct::build(vk::AccelerationStructureGeometryKHR& geometry, const vk::AccelerationStructureTypeKHR& type, uint32_t primitiveCount)
+{
+    this->type = type;
+    this->primitiveCount = primitiveCount;
+
+    buildGeometryInfo.setType(type);
+    buildGeometryInfo.setFlags(vk::BuildAccelerationStructureFlagBitsKHR::ePreferFastTrace);
+    buildGeometryInfo.setGeometries(geometry);
+
+    getSizes();
+    createBuffer();
+    createAccelStruct();
+    createScratchBuffer();
+
+    buildGeometryInfo.setDstAccelerationStructure(*accelStruct);
+    buildGeometryInfo.setScratchData(scratchBuffer.getDeviceAddress());
+
+    vk::AccelerationStructureBuildRangeInfoKHR buildRangeInfo{};
+    buildRangeInfo
+        .setPrimitiveCount(primitiveCount)
+        .setPrimitiveOffset(0)
+        .setFirstVertex(0)
+        .setTransformOffset(0);
+
+    auto cmdBuf = context->getDevice().beginGraphicsCommand();
+    cmdBuf->buildAccelerationStructuresKHR(buildGeometryInfo, &buildRangeInfo);
+    context->getDevice().endGraphicsCommand(*cmdBuf);
+
+    deviceAddress = context->getVkDevice().getAccelerationStructureAddressKHR({ *accelStruct });
+}
+
+void vkt::AccelStruct::createBuffer()
+{
+    buffer.initialize(*context, size,
+                      vkBU::eAccelerationStructureStorageKHR | vkBU::eShaderDeviceAddress,
+                      vkMP::eDeviceLocal);
+}
+
+void vkt::AccelStruct::createAccelStruct()
+{
+    vk::AccelerationStructureCreateInfoKHR createInfo;
+    createInfo.setBuffer(buffer.get());
+    createInfo.setSize(size);
+    createInfo.setType(type);
+    accelStruct = context->getVkDevice().createAccelerationStructureKHRUnique(createInfo);
+}
+
+void vkt::AccelStruct::createScratchBuffer()
+{
+    scratchBuffer.initialize(*context,
+                             scratchSize,
+                             vkBU::eStorageBuffer | vkBU::eShaderDeviceAddress,
+                             vkMP::eDeviceLocal);
+}
+
+void vkt::AccelStruct::getSizes()
+{
+    auto buildSizesInfo = context->getVkDevice().getAccelerationStructureBuildSizesKHR(
+        vk::AccelerationStructureBuildTypeKHR::eDevice, buildGeometryInfo, primitiveCount);
+    size = buildSizesInfo.accelerationStructureSize;
+    scratchSize = buildSizesInfo.buildScratchSize;
 }
